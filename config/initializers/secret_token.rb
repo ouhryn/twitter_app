441d1335838390c4504eb075dec66012b9cf6bdb
Oleh Uhryn <olehuhryn@gmail.com>
2013-10-07 21:24:11 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TwitterApp::Application.config.secret_token = 'a4af523a597535ba8fde97f073f3639a35656a7f8ac852c3fa70a103a23a7152f9a03e7fe3300ab6738a313d2b15e120a45217df57c6070f5cdaef024465d74c'
