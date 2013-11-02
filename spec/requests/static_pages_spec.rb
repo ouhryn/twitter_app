f5f73bcee4e2e402461c0d2bc9210ad0a79514dd
ouhryn <olehuhryn@gmail.com>
2013-11-02 18:18:17 +0200

    Finish with static pages rspec

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,36 +1,67 @@
 require 'spec_helper'
 
-subject { page }
-
 describe "Static pages" do
 
   describe "Home page" do
   	before {visit root_path}
 
-    it { should have_selector('h1', :text =>'Sample App') }
-    it { should have_selector('title', :text => full_title('')) }
-    it { should_not have_selector('title', :text => '| Home') }
-  end
+    it "should have h1 'Sample' App" do
+    page.should have_selector('h1', :text =>'Sample App') 
+     end
+    it "should have title 'Sample' App" do
+      page.should have_selector('title', :text => full_title('')) 
+     end
+    it "should not have a custom page title" do
+     page.should_not have_selector('title', :text => '| Home') 
+ end
+     end
 
   describe "Help page" do
    before {visit help_path}
 
-   it { should have_selector('h1', :text =>'Help') }
-   it { should have_selector('title', :text => "#{base_title} | Help") }
+   it "should have h1 'Help' App" do
+    page.should have_selector('h1', :text =>'Help') 
+    end
+   it "should have title 'Help' App" do
+   page.should have_selector('title', :text => full_title('Help'))
+    end
   end
 
   describe "About page" do
    before {visit about_path}
 
-   it { should have_selector('h1', :text =>'About Us') }
-   it { should have_selector('title', :text => "#{base_title} | About Us") }
+   it "should have h1 'About' App" do
+   page.should have_selector('h1', :text =>'About Us')
+end
+   it "should have title 'About' App" do
+    page.should have_selector('title', :text => full_title('About'))
+end
   end
 
  describe "Contact page" do
   before {visit contact_path}
 
-  it { should have_selector('h1', :text => 'Contact') }
-  it { should have_selector('title', :text => "#{base_title} | Contact") }
+  it "should have h1 'Contact' App" do
+  page.should have_selector('h1', :text => 'Contact')
+end
+  it "should have title 'Contact' App" do
+   page.should have_selector('title', :text => full_title('Contact'))
+end
  end
+  
+it "should have the right links on the layout" do
+ visit root_path
+ click_link "About"
+ page.should have_selector 'title', text: full_title('About Us')
+ click_link "Help"
+ page.should have_selector 'title', text: full_title('Help') 
+ click_link "Contact"
+ page.should have_selector 'title', text: full_title('Contact')
+ click_link "Home"
+ #click_link "Sign up now!"
+ #page.should have_selector 'title', text: full_title('Sign up')
+ click_link "sample app"
+ page.should have_selector 'title', text: full_title('')
+end
 
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,13 +1,15 @@
 require 'spec_helper'
 
 describe "User pages" do
- 
-subject { page }
 
 describe "signup page" do
   before { visit signup_path }
 
-  it { should have_selector('h1', text: 'Sign up') }
-  it { should have_selector('title', text: full_title('Sign up')) }
+  it "should have h1 'Sign up'" do
+  page.should have_selector('h1', text: 'Sign up')
+end
+  it "should have title 'Sign up'" do
+  page.should have_selector('title', text: full_title('Sign up'))
+end
 	end
 end
